2e3e8371c34bebd46bdc79fc7778d8f250d87f0c
neodarz <neodarz@neodarz.net>
2020-08-15 15:49:33 +0200

    Convert filename to path before try to open it

--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
 import sys
 import logging
+from pathlib import Path
 
 
 def read_file(filename):
     lines = []
+    filename = Path(filename)
 
     if filename.is_dir():
         logging.fatal(f'{filename} is a folder instead of a file!')
